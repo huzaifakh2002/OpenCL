3d2805da97b2e39c0656fdffb5553a80ee6148b1
huzaifakh2002 <168071752+huzaifakh2002@users.noreply.github.com>
2024-05-05 20:21:44 +0500

    host.c

--- host.c
+++ host.c
@@ -1,21 +1,7 @@
 #include <CL/cl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "stb_image.h"
-#include "stb_image_write.h"
-
-// OpenCL kernel to convert RGB to grayscale
-const char* kernelCode = 
-"__kernel void rgb_to_gray(__global const uchar *input, __global uchar *output, int width, int height, int channels) {\n"
-"    int x = get_global_id(0);\n"
-"    int y = get_global_id(1);\n"
-"    int idx = (y * width + x) * channels;\n"
-"    uchar b = input[idx];\n"
-"    uchar g = input[idx + 1];\n"
-"    uchar r = input[idx + 2];\n"
-"    uchar gray = (uchar)(0.299f * r + 0.587f * g + 0.114f * b);\n"
-"    output[y * width + x] = gray;\n"
-"}\n";
+#include <string.h>
 
 void checkError(cl_int err, const char* message) {
     if (err != CL_SUCCESS) {
@@ -24,16 +10,99 @@ void checkError(cl_int err, const char* message) {
     }
 }
 
+#pragma pack(push, 1)
+typedef struct {
+    unsigned char  bfType[2];
+    unsigned int   bfSize;
+    unsigned short bfReserved1;
+    unsigned short bfReserved2;
+    unsigned int   bfOffBits;
+} BMPHeader;
+
+typedef struct {
+    unsigned int   biSize;
+    int            biWidth;
+    int            biHeight;
+    unsigned short biPlanes;
+    unsigned short biBitCount;
+    unsigned int   biCompression;
+    unsigned int   biSizeImage;
+    int            biXPelsPerMeter;
+    int            biYPelsPerMeter;
+    unsigned int   biClrUsed;
+    unsigned int   biClrImportant;
+} BMPInfoHeader;
+#pragma pack(pop)
+
+void loadBMP(const char* filename, unsigned char** imageData, BMPInfoHeader* bmpInfoHeader) {
+    FILE* file = fopen(filename, "rb");
+    if (!file) {
+        fprintf(stderr, "Error: Unable to open file %s.\n", filename);
+        exit(1);
+    }
+
+    BMPHeader bmpHeader;
+    fread(&bmpHeader, sizeof(BMPHeader), 1, file);
+    fread(bmpInfoHeader, sizeof(BMPInfoHeader), 1, file);
+
+    if (bmpHeader.bfType[0] != 'B' || bmpHeader.bfType[1] != 'M') {
+        fprintf(stderr, "Error: Not a BMP file.\n");
+        exit(1);
+    }
+
+    size_t dataSize = bmpInfoHeader->biSizeImage;
+    *imageData = (unsigned char*)malloc(dataSize);
+    fseek(file, bmpHeader.bfOffBits, SEEK_SET);
+    fread(*imageData, dataSize, 1, file);
+
+    fclose(file);
+}
+
+void saveBMP(const char* filename, const unsigned char* imageData, const BMPInfoHeader* bmpInfoHeader) {
+    FILE* file = fopen(filename, "wb");
+    if (!file) {
+        fprintf(stderr, "Error: Unable to open file %s for writing.\n", filename);
+        exit(1);
+    }
+
+    BMPHeader bmpHeader = {
+        {'B', 'M'},
+        (unsigned int)(sizeof(BMPHeader) + sizeof(BMPInfoHeader) + bmpInfoHeader->biSizeImage),
+        0,
+        0,
+        (unsigned int)(sizeof(BMPHeader) + sizeof(BMPInfoHeader))
+    };
+
+    fwrite(&bmpHeader, sizeof(BMPHeader), 1, file);
+    fwrite(bmpInfoHeader, sizeof(BMPInfoHeader), 1, file);
+    fwrite(imageData, bmpInfoHeader->biSizeImage, 1, file);
+
+    fclose(file);
+}
+
+const char* kernelCode = 
+"__kernel void rgb_to_gray(__global const uchar *input, __global uchar *output, int width, int height, int channels) {\n"
+"    int x = get_global_id(0);\n"
+"    int y = get_global_id(1);\n"
+"    int idx = (y * width + x) * channels;\n"
+"    uchar blue = input[idx];\n"
+"    uchar green = input[idx + 1];\n"
+"    uchar red = input[idx + 2];\n"
+"    uchar gray = (uchar)(0.299f * red + 0.587f * green + 0.114f * blue);\n"
+"    int gray_idx = y * width + x;\n"
+"    output[gray_idx] = gray;\n"
+"}\n";
+
 int main() {
     cl_int err;
 
-    // Load JPG image using stb_image
-    int width, height, channels;
-    unsigned char* colorImage = stbi_load("image.jpg", &width, &height, &channels, 3); // Load as RGB
-    if (colorImage == NULL) {
-        fprintf(stderr, "Error: Could not load JPG image.\n");
-        return -1;
-    }
+    unsigned char* imageData;
+    BMPInfoHeader bmpInfoHeader;
+    loadBMP("image.bmp", &imageData, &bmpInfoHeader);
+
+    int width = bmpInfoHeader.biWidth;
+    int height = bmpInfoHeader.biHeight;
+    int channels = bmpInfoHeader.biBitCount / 8;
 
     // Get OpenCL platforms
     cl_platform_id platform;
@@ -70,12 +139,14 @@ int main() {
     }
 
     // Create buffers
-    cl_mem inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY, width * height * channels, NULL, &err);
+    cl_mem inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY, bmpInfoHeader.biSizeImage, NULL, &err);
+    checkError(err, "Creating input buffer");
+
     cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, width * height, NULL, &err);
-    checkError(err, "Creating buffers");
+    checkError(err, "Creating output buffer");
 
     // Copy image data to the input buffer
-    err = clEnqueueWriteBuffer(queue, inputBuffer, CL_TRUE, 0, width * height * channels, colorImage, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(queue, inputBuffer, CL_TRUE, 0, bmpInfoHeader.biSizeImage, imageData, 0, NULL, NULL);
     checkError(err, "Writing to input buffer");
 
     // Create the kernel and set its arguments
@@ -94,17 +165,16 @@ int main() {
 
     // Execute the kernel
     err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, globalWorkSize, NULL, 0, NULL, NULL);
-    checkError(err, "Enqueueing NDRange kernel");
+    checkError(err, "Executing kernel");
 
     unsigned char* grayImage = (unsigned char*)malloc(width * height);
     err = clEnqueueReadBuffer(queue, outputBuffer, CL_TRUE, 0, width * height, grayImage, 0, NULL, NULL);
     checkError(err, "Reading from output buffer");
 
-    // Save the grayscale image using stb_image_write
-    stbi_write_jpg("gray_image.jpg", width, height, 1, grayImage, 90); // 90 is the quality level
+    saveBMP("gray_image.bmp", grayImage, &bmpInfoHeader);
 
     // Cleanup
-    free(colorImage);
+    free(imageData);
     free(grayImage);
     clReleaseKernel(kernel);
     clReleaseProgram(program);
@@ -117,4 +187,3 @@ int main() {
 
     return 0;
 }
-
